3d93f7e320811b9e9ff11b65ebbdeb222fd41b17
Ravil <ravilka.zftsh@yandex.ru>
2025-06-18 08:29:06 +0300

    - Include json library - Serialization template

--- NauEngine_Demo/Scene.cpp
+++ NauEngine_Demo/Scene.cpp
@@ -1,7 +1,21 @@
 #include "Scene.h"
+#include <fstream>
 
 Scene::Scene()
 {
+    json data;
+    /*
+    std::ofstream outfstream("scene_out.json");
+    stuff_to_json(data, Json_stuff({4,1,4}));
+    outfstream << std::setw(4) << data << std::endl;
+    */
+    std::ifstream f("scene_out.json");
+    f >> data;
+    Json_stuff t2;
+    t2.from_json(data);
+    std::cout << t2.x << " " << t2.y << " " << t2.z << "\n";
+    // = data.get<Json_stuff>(); // גûחûגאוע from_json
+
 }
 
 Scene::~Scene()
--- NauEngine_Demo/Scene.h
+++ NauEngine_Demo/Scene.h
@@ -2,12 +2,30 @@
 
 #include <vector>
 #include "Entity.h"
+#include <nlohmann_json/json.hpp>
+
+using json = nlohmann::json;
 
 class Scene
 {
 public:
     Scene();
     ~Scene();
+
+    class Json_stuff {
+    public:
+        Json_stuff() {};
+        float x = 0, y = 0, z = 0;
+
+        void to_json(json& j) {
+            j = json{ {"x", this->x}, {"y", this->y}, {"z", this->z} };
+        }
+        void from_json(const json& j) {
+            j.at("x").get_to(this->x);
+            j.at("y").get_to(this->y);
+            j.at("z").get_to(this->z);
+        }
+    };
 
     // To-do smart ptr?
     void AddEntity(Entity* node);
@@ -17,5 +35,8 @@ public:
 
     // To-do smart ptr?
     std::vector<Entity*> entities;
+
+    // Serialization
+    
 };
 
